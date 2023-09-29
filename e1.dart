036ccc78189c8ce653825d2de7d65da3a238ca3e
Ebrahim683 <ebrahimrahman20@gmail.com>
2023-09-29 15:09:11 +0600

    added e1 and e2 class

---
+++ e1.dart
@@ -0,0 +1,8 @@
+class E1 {
+  void printList() {
+    List l1 = [1, 5, 4, 8, 8, 56, 5, 5, 'a', 'b', 'd', 'fx'];
+    for (var element in l1) {
+      print(element);
+    }
+  }
+}
--- main.dart
+++ main.dart
@@ -1,4 +1,10 @@
+import 'e1.dart';
+
 void main(List<String> args) {
   print('Hello Dart!');
   print('change by main');
+
+  //list created by ebrahim
+  E1 e1 = E1();
+  e1.printList();
 }
