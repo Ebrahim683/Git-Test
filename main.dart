edc25603a85b400d1f0731c500e305331be02de3
Ebrahim683 <ebrahimrahman20@gmail.com>
2023-09-29 15:15:27 +0600

    rahman R1 class created

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'e1.dart';
+import 'r1.dart';
 
 void main(List<String> args) {
   print('Hello Dart!');
@@ -7,4 +8,8 @@ void main(List<String> args) {
   //list created by ebrahim
   E1 e1 = E1();
   e1.printList();
+
+  //list created by rahman
+  R1 r1 = R1();
+  r1.sortData();
 }
---
+++ r1.dart
@@ -0,0 +1,6 @@
+class R1 {
+  sortData() {
+    List<int> l1 = <int>[5, 7, 4, 9, 6, 3, 8, 1, 2, 0];
+    print(l1..sort());
+  }
+}
